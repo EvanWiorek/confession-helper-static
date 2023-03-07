c6f8c3e7ec3247ee5b95b79e6b52115b5ff5cb89
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-03-07 07:46:11 -0600

    confession-helper

---
+++ script.js
@@ -0,0 +1,38 @@
+//Scaling transition for desktop navbar link elements
+function scale(element, value) {
+  element.style.transform = "scale(" + value + ")";
+}
+
+//Mobile Navbar Functionality
+function mobileNav() {
+  var x = document.getElementById("mobile-links");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+
+//Accordian Controls
+var acc = document.getElementsByClassName("accordion");
+var commandmentTenTitle = document.getElementById("commandment-10-title")
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } 
+    else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+    if (this.classList.contains("active")) {
+      commandmentTenTitle.style = "border-radius: 0px;";
+    }
+    else {
+      this.style = "";
+    }
+  });
+}
