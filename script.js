5bbeeee3adfbdc110db1de6840036a6b8e0a0008
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-03-08 19:36:41 -0600

    update

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function scale(element, value) {
 function mobileNav() {
   var x = document.getElementById("mobile-links");
   if (x.style.display === "block") {
-      x.style.display = "none";
+    x.style.display = "none";
   } else {
-      x.style.display = "block";
+    x.style.display = "block";
   }
 }
 
@@ -19,12 +19,12 @@ var commandmentTenTitle = document.getElementById("commandment-10-title")
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
-    } 
+    }
     else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
@@ -36,3 +36,17 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//adding checked items to the confessions list
+var confessionItem = document.getElementById("confession-item");
+var listItem = document.getElementById("list-item")
+var confessionList = document.getElementById("confession-list")
+
+document.querySelector('#Checkbox').addEventListener('click', (event) => {
+  if (event.target.checked) {
+    // confessionItem.style.display = "block";
+    confessionList.innerHTML += "<li id='list-item'>Chicken Nuggets</li>";
+    listItem.innerText = confessionItem.innerText;
+    console.log("Item has been checked");
+  }
+})
